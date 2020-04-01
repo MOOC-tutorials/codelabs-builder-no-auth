92e18a563491115c13a4eb9989256ed512a727b9
Glitch (codelabs-builder-no-auth) <none>
2020-04-01 21:12:24 +0000

    🏊🏼📍 Updated with Glitch

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const directory = require('serve-index');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
